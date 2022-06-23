d7aa67fdc9ee0545a8d6c4b039b87925398bfb68
Perry Nelson <peregrino.nelson@gmail.com>
2022-06-23 09:21:03 +0000

    Making whatsapp number configurable

--- collection/functions/messaging/send_prompt.private.js
+++ collection/functions/messaging/send_prompt.private.js
@@ -1,7 +1,12 @@
 exports.sendPrompt = async (context, recipient, text, media) => {
+    let varsPath = Runtime.getFunctions()['vars_helper'].path;
+    let varsHelper = require(varsPath);
+
+    let whatsappNumber = varsHelper.getVar("whatsapp-number");
+
     let request = {
         to: recipient.startsWith('whatsapp') ? recipient : `whatsapp:${recipient.startsWith("+") ? recipient : "+" + recipient}`,
-        from: 'whatsapp:+14155238886',
+        from: `whatsapp:${whatsappNumber}`,
         body: text,
         mediaUrl: media,
     };
